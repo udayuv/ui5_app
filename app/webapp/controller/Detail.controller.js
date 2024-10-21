6cd727f83e7f193bec4b0d80b0b83dc3b1521276
I570425 <uday.yadav@sap.com>
2024-10-21 20:05:03 +0530

    Step 32: Routing Back and History

--- app/webapp/controller/Detail.controller.js
+++ app/webapp/controller/Detail.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], (Controller) => {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], (Controller, History) => {
 	"use strict";
 
 	return Controller.extend("ui5.walkthrough.controller.Detail", {
@@ -14,6 +15,18 @@ sap.ui.define([
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
 			});
+		},
+
+        onNavBack() {
+			const oHistory = History.getInstance();
+			const sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				const oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
 		}
 	});
 });
